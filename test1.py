8dfc29d13881c28f61d8c3ee560bcc83af8ed2c4
zora89 <zorawar.purohit@gmail.com>
2022-03-11 03:32:31 +0530

    added query with 'and' use, along with user id - needs more understanding

--- test1.py
+++ test1.py
@@ -3,8 +3,25 @@ import api_token
 
 client = tweepy.Client(bearer_token=api_token.BEARER_TOKEN)
 
-query = 'covid -is:retweet'
+query = 'andaman "and" bjp -is:retweet'
 
-response = client.search_recent_tweets(query=query, max_results=10)
+response = client.search_recent_tweets(query=query, max_results=10, tweet_fields=['created_at', 'lang'], user_fields=['profile_image_url'], expansions=['author_id'] )
 
-print(response)
+
+users = {u['id']: u for u in response.includes['users']}
+
+count = 0
+for tweet in response.data:
+    if users[tweet.author_id]:
+        user = users[tweet.author_id ]
+        count += 1
+        print("--")
+        print(str(count))
+        print (str(tweet.id))
+        print("------->>>>>>>>")
+        print(tweet.text)
+        print("------->>>>>>>>")
+        print(tweet.lang)
+        print(str(tweet.created_at))
+        print(user.username)
+        print(user.profile_image_url)
