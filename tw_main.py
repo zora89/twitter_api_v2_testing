e24f3d3bad4378bd70c40360943c195e536fdd8b
zora89 <zorawar.purohit@gmail.com>
2022-03-11 04:42:04 +0530

    final files

---
+++ tw_main.py
@@ -0,0 +1,33 @@
+import  tweepy
+import api_token 
+
+client = tweepy.Client(bearer_token=api_token.BEARER_TOKEN)
+
+query = 'ukraine "and" victory "and" retreat -is:retweet'
+
+#checks for recent tweets
+response = client.search_recent_tweets(query=query, max_results=100, tweet_fields=['created_at', 'lang'], user_fields=['profile_image_url'], expansions=['author_id'] )
+#checks for count on of tweets in past week on said query
+counts = client.get_recent_tweets_count(query=query, granularity = 'day')
+#needs to be UNDERSTOOD
+users = {u['id']: u for u in response.includes['users']}
+
+count = 0
+for tweet in response.data:
+    if users[tweet.author_id]:
+        user = users[tweet.author_id ]
+        count += 1
+        print("--")
+        print(str(count))
+        print (str(tweet.id))
+        print("------->>>>>>>>")
+        print(tweet.text)
+        print("------->>>>>>>>")
+        print(tweet.lang)
+        print(str(tweet.created_at))
+        print(user.username)
+        print(user.profile_image_url)
+
+
+for tweet in counts.data:
+    print(tweet['tweet_count'])
--- user_tweets.py
+++ user_tweets.py
@@ -3,18 +3,15 @@ import api_token
 
 client = tweepy.Client(bearer_token=api_token.BEARER_TOKEN)
 
-#put userid @ here ->
-users = client.get_users(usernames=['elonmusk'])
+tweets = client.get_users_tweets(id=api_token.elonmusk, tweet_fields=['created_at'], max_results=10)
 
-tweets = client.get_users_tweets(id=api_token.runa_tw_id, tweet_fields=['created_at'], max_results=100)
-
-for user in users:
-    print(user)
-
+count = 0
 for tweet in tweets.data:
+    count += 1
     print(tweet.id)
     print("------->>>>>>>>")
     print(tweet.text)
     print("------->>>>>>>>")
     print(tweet.created_at)
+    print("---" + str(count) + "--->")
 
