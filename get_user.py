4ed1c15dfa810a72b20113ec2cfc3005235d0a72
zora89 <zorawar.purohit@gmail.com>
2022-03-11 23:41:33 +0530

    query refined

--- get_user.py
+++ get_user.py
@@ -4,17 +4,9 @@ import api_token
 client = tweepy.Client(bearer_token=api_token.BEARER_TOKEN)
 
 #put userid @ here ->
-users = client.get_users(usernames=['elonmusk'])
-
-tweets = client.get_users_tweets(id=api_token.runa_tw_id, tweet_fields=['created_at'], max_results=100)
+users = client.get_users(usernames=['zorawarpurohit'])
 
 for user in users:
     print(user)
 
-for tweet in tweets.data:
-    print(tweet.id)
-    print("------->>>>>>>>")
-    #print(tweet.text)
-    print("------->>>>>>>>")
-    print(tweet.created_at)
 
