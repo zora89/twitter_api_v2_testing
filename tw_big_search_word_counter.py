08453b7b404ddda7db322cf5eb2fbf286aeaa0e1
zora89 <zorawar.purohit@gmail.com>
2022-03-12 05:40:18 +0530

    refactored

--- tw_big_search_word_counter.py
+++ tw_big_search_word_counter.py
@@ -1,27 +1,37 @@
+from pickle import NONE
 import tweepy
 import api_token
 
+#setting the client, query and response always first.
 client = tweepy.Client(bearer_token=api_token.BEARER_TOKEN)
+query = 'india -is:retweet -#eximbank -btc -eth -nft -crypto -donation -donating -donate lang:en '
+response = tweepy.Paginator(
+        client.search_recent_tweets, query=query, max_results=100, 
+        tweet_fields=['created_at', 'lang'], 
+        user_fields=['profile_image_url'], 
+        expansions=['author_id']).flatten(limit=1000
+        )
 
-query = 'andaman  -is:retweet -#eximbank -btc -eth -nft -crypto -donation -donating -donate lang:en '
 #case sensitive word check
-word_check_1 = "airpoRT"
-word_check_2 = "TOURISM"
+word_check_1 = "love"
+word_check_2 = "weapon"
 
+#setting counters for ease of data relay
 count = 0
 word_count_1 = 0
 word_count_2 = 0
-for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, max_results=100).flatten(limit=1000):
+for tweet in response:
+        
     count+=1
     print(count)
-    unified_lowercase_tweet = tweet.text.lower()
+    lowercase_tweet = tweet.text.lower()
     #enter the word you wish to count occurence of in each tweet !
-    word_alert = unified_lowercase_tweet.count(word_check_1.lower())
-    versus_word_alert = unified_lowercase_tweet.count(word_check_2.lower())
-    print(f" '--->>>{word_check_1}' repeated {word_alert} times")
-    print(f" '--->>>{word_check_2}' repeated {versus_word_alert} times")
-    word_count_1 += word_alert
-    word_count_2 += versus_word_alert
+    word_1_alert = lowercase_tweet.count(word_check_1.lower())
+    word_2_alert = lowercase_tweet.count(word_check_2.lower())
+    print(f" '--->>>{word_check_1}' repeated {word_1_alert} times")
+    print(f" '--->>>{word_check_2}' repeated {word_2_alert} times")
+    word_count_1 += word_1_alert
+    word_count_2 += word_2_alert
     print(tweet.text)
     print("\n")
     print("\n")
